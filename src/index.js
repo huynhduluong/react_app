9b08b7e29a139c37b55fe80c9091ac2590c6c3c5
Huynh Du Luong <huynhduluong@gmail.com>
2020-09-27 15:18:12 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  //nội dung in ra html
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  //vị trí cần render ra
   document.getElementById('root')
 );
 
