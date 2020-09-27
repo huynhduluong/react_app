45ee321b300b23697850257b3074a560d9d304c3
Huynh Du Luong <huynhduluong@gmail.com>
2020-09-27 15:50:07 +0700

    finish component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StateLessComponent from './components/StateLessComponent';
+import StateFull, { StateFullComponent } from './components/StateFullComponent';
 
 function App() {
   return (
-    <div>My app</div>
+    <div>
+      <StateLessComponent />
+      <StateFullComponent />
+      <StateFull />
+    </div>
   );
 }
 
---
+++ src/components/StateFullComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export class StateFullComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Đây là component dưới dạng class</h2>
+            </div>
+        );
+    }
+}
+
+export default StateFullComponent;
